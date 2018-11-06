037f4a224b29350c729a8733847524061803962b
Dan Winter <dan.j.winter@gmail.com>
2018-11-06 16:17:30 -0700

    Change function name, pull out latitude and longitude querystrings

--- handler.js
+++ handler.js
@@ -2,13 +2,22 @@
 
 /* eslint-disable no-param-reassign */
 
-module.exports.hello = function (context) {
+module.exports.localTrucks = function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
+  if (req.query && req.query.latitude && req.query.longitude) {
+    const lat = req.query.latitude
+    const long = req.query.longitude
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: `Latitude: ${lat}, longitude: ${long}`,
+    };
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: 'Go Serverless v1.x! Your function executed successfully!',
-  };
+  } else {
+    context.res = {
+      body: 'Please supple a longitude and latitude as query parameters'
+    }
+  }
+
 
   context.done();
 };
