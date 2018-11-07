22758361c644fe48f4d115c6abc9a6f4b99765c5
Dan Winter <dan.j.winter@gmail.com>
2018-11-06 17:10:21 -0700

    basic soda query

--- handler.js
+++ handler.js
@@ -1,15 +1,29 @@
-'use strict';
+const soda = require('soda-js');
 
-/* eslint-disable no-param-reassign */
+function sodaQuery() {
+  return new Promise((res, rej) => {
+    const consumer = new soda.Consumer('data.sfgov.org');
+    consumer.query()
+      .withDataset('6a9r-agq8')
+      .limit(5)
+      .where({status: 'APPROVED'})
+      .getRows()
+        .on('success', function(rows) { res(rows); })
+        .on('error', function(error) { rej(error); });
+  
+  })
+}
 
-module.exports.localTrucks = function (context, req) {
+module.exports.localTrucks = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
   if (req.query && req.query.latitude && req.query.longitude) {
-    const lat = req.query.latitude
-    const long = req.query.longitude
+    // const lat = req.query.latitude
+    // const long = req.query.longitude
     context.res = {
       // status: 200, /* Defaults to 200 */
-      body: `Latitude: ${lat}, longitude: ${long}`,
+      body: {
+        trucks: await sodaQuery()
+      }
     };
 
   } else {
